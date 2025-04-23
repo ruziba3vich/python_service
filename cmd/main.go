a7f78e66d4f959154984d9404667768bccca3f30
ruziba3vich <rruzibayevich@gmail.com>
2025-04-23 19:18:11 +0500

    feat python_service: create app starter point

---
+++ cmd/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"github.com/ruziba3vich/python_service/genprotos/genprotos/compiler_service"
+	"github.com/ruziba3vich/python_service/internal/service"
+	"go.uber.org/fx"
+	"google.golang.org/grpc"
+)
+
+func NewNetListener(lc fx.Lifecycle) (net.Listener, error) {
+	lis, err := net.Listen("tcp", ":7771")
+	if err != nil {
+		return nil, err
+	}
+
+	lc.Append(fx.Hook{
+		OnStop: func(ctx context.Context) error {
+			return lis.Close()
+		},
+	})
+
+	return lis, nil
+}
+
+func main() {
+	fx.New(
+		fx.Provide(
+			NewNetListener,
+			service.NewPythonExecutorServer,
+		),
+		fx.Invoke(func(lc fx.Lifecycle, lis net.Listener, server *service.PythonExecutorServer) {
+			s := grpc.NewServer()
+			compiler_service.RegisterCodeExecutorServer(s, server)
+
+			lc.Append(fx.Hook{
+				OnStart: func(ctx context.Context) error {
+					log.Println("gRPC server starting on :7771...")
+					go func() {
+						if err := s.Serve(lis); err != nil {
+							log.Printf("gRPC server failed: %v", err)
+						}
+					}()
+					return nil
+				},
+				OnStop: func(ctx context.Context) error {
+					log.Println("Stopping gRPC server...")
+					s.GracefulStop()
+					return nil
+				},
+			})
+		}),
+	).Run()
+}
--- go.mod
+++ go.mod
@@ -4,11 +4,15 @@ go 1.24.2
 
 require (
 	github.com/google/uuid v1.6.0
+	go.uber.org/fx v1.23.0
 	google.golang.org/grpc v1.72.0
 	google.golang.org/protobuf v1.36.6
 )
 
 require (
+	go.uber.org/dig v1.18.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
 	golang.org/x/net v0.35.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
