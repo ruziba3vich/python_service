92c221f3d5b42c6d0f87377dfcb29307c7916bd7
ruziba3vich <rruzibayevich@gmail.com>
2025-04-24 02:06:53 +0500

    fix python_service: fix logging service proto generator path

--- generate_logging_protos.sh
+++ generate_logging_protos.sh
@@ -11,6 +11,6 @@ mkdir -p $OUT_DIR
 protoc -I=$PROTO_DIR \
   --go_out=$OUT_DIR \
   --go-grpc_out=$OUT_DIR \
-  $PROTO_DIR/loggig_protos/logging.proto
+  $PROTO_DIR/logging_protos/logging.proto
 
 echo "protos generated successfully"
